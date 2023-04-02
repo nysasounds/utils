ab4ccd46a73ec0ef2b8b40257351a416fd96bd9c
jcat <jcat@nysasounds.co.uk>
2023-04-02 10:32:09 +0000

    backup: make it more generic, with most config moved to backup location

--- backup/backup-sys.sh
+++ backup/backup-sys.sh
@@ -1,47 +1,49 @@
 #!/bin/bash
 
-BAKLABEL=${1}
-BAKLABEL="${BAKLABEL:-"full-sys"}"
+set -u
 
-MARCHAGE="365"
-WARCHAGE="31"
+# Location of backup store and config #
+BAK_STORE="${1:-/this/path/does/not/exit}"
+BAK_EXCLD="${BAK_STORE}/sys.xcld"
+BAK_LIST="${BAK_STORE}/sys.list"
 
-HOSTN=`/bin/hostname`
+# Optional label for backup set #
+BAK_LABEL="${2:-"WEEKLY"}"
 
-DATESTAMP=`/bin/date +"%Y-%m-%d_%H-%M"`
-export STARTTIME=`/bin/date -u +"%s"`
+# Defatul times for retension of weekly or monthly backups #
+MONTHLY_ARCHIVE_AGE="365"
+WEEKLY_ARCHIVE_AGE="31"
 
-BAKSTORE="/home/data/backup/sys/"
-EXCLD="${BAKSTORE}/sys.xcld"
-BACKPRE="${HOSTN}"
-BAKBOOT="${BAKSTORE}/${BACKPRE}-boot_${BAKLABEL}_${DATESTAMP}.tar.gz"
-BAKROOT="${BAKSTORE}/${BACKPRE}-root_${BAKLABEL}_${DATESTAMP}.tar.gz"
-BAKHOME="${BAKSTORE}/${BACKPRE}-home_${BAKLABEL}_${DATESTAMP}.tar.gz"
-BAKVAR="${BAKSTORE}/${BACKPRE}-var_${BAKLABEL}_${DATESTAMP}.tar.gz"
-LOG="${BAKSTORE}/${BACKPRE}_${BAKLABEL}_${DATESTAMP}.log"
+DATE_STAMP=$(/bin/date +"%Y-%m-%d_%H-%M")
+export START_TIME=$(/bin/date -u +"%s")
 
-EXITSTAT=0
+BAK_PREFIX="${BAK_PREFIX:-$(/bin/hostname)}"
+
+LOG="${BAK_STORE}/${BAK_PREFIX}_${BAK_LABEL}_${DATE_STAMP}.log"
+
+MOUNT_BOOT="no"
+
+EXIT_STATUS=0
 
 
 header() {
 
 cat << END
 
-Backup up system: ${HOSTN}
+Backup up prefix: ${BAK_PREFIX}
 
-Archive:      $BAKBOOT
-Archive:      $BAKROOT
-Archive:      $BAKHOME
-Archive:      $BAKVAR
+Configs:
+$(cat ${BAK_LIST})
+
 Log:          $LOG
 
-Start Time:   $DATESTAMP
+Start Time:   $DATE_STAMP
 
 Excluding:
-`cat $EXCLD`
+$(cat "${BAK_EXCLD}")
 
 Currently mounted:
-`/bin/mount`
+$(/bin/mount)
 
 END
 
@@ -50,50 +52,65 @@ END
 
 backup() {
 
-echo "Changing to /" ; pushd /
+BAK_FILE="${BAK_STORE}/${BAK_PREFIX}-${BAK_NAME}_${BAK_LABEL}_${DATE_STAMP}.tar.gz"
+
+echo "Starting ${BAK_NAME} ${BAK_SOURCE}"
+/bin/tar zcvf "${BAK_FILE}" -X "${BAK_EXCLD}" "${BAK_SOURCE}" --one-file-system 2>&1
+
+}
+
+
+backups() {
+
+echo "Changing to /"
+pushd /
 if [ $? -ne 0 ] ; then
 	echo "Couldn't switch to / , exiting..."
-	EXITSTAT=1
-else
-	/bin/mount |grep -Eq "\ on\ \/boot\ " && MBOOT=yes
-	if [ "$MBOOT" != "yes" ] ; then
-		echo "Mounting /boot"
-		/bin/mount /boot
-	fi
-	echo "Starting /boot"
-	/bin/tar zcvf $BAKBOOT -X $EXCLD /boot --one-file-system 2>&1 || BSTAT=$?
-	echo "Starting /"
-	/bin/tar zcvf $BAKROOT -X $EXCLD / --one-file-system 2>&1 || BSTAT=$?
-	echo "Starting /home"
-	/bin/tar zcvf $BAKHOME -X $EXCLD /home --one-file-system 2>&1 || BSTAT=$?
-	echo "Starting /var"
-	/bin/tar zcvf $BAKVAR -X $EXCLD /var --one-file-system 2>&1 || BSTAT=$?
-	export BAKTIME=$((`/bin/date -u "+%s"` - $STARTTIME ))
-	BAKDATE=`/bin/date -u -d "1970-01-01 $BAKTIME secs" +"%R:%S"`
-	echo "Total Backup Time: $BAKDATE"
-	echo
-	if [ "$MBOOT" != "yes" ] ; then
-		echo "Unmounting /boot"
-		/bin/umount /boot
-	fi
-	if [ -n "$BSTAT" ] ; then
-		echo "Backup seemed to fail :("
-		echo "Exit status was: $BSTAT"
-		false
-	else
-		echo "Backup completed OK"
-		echo
-		echo "Changing from root" ; popd
-		rotate
-	fi
-	EXITSTAT=$?
-	echo
-	times
-	echo
-
+	EXIT_STATUS=1
+	return $EXIT_STATUS
 fi
 
-return $EXITSTAT
+/bin/mount |grep -Eq "\ on\ \/boot\ " && MOUNT_BOOT="yes"
+if [ "${MOUNT_BOOT}" != "yes" ] ; then
+	echo "Mounting /boot"
+	/bin/mount /boot
+fi
+
+# Do backups #
+BAK_STATUS=0
+cat "${BAK_LIST}" | while read l ; do
+	BAK_NAME=${l%% *}
+	BAK_SOURCE=${l#* }
+	backup || BAK_STATUS=1
+done
+
+export BAK_TIME=$((`/bin/date -u "+%s"` - $START_TIME ))
+BAK_DATE=$(/bin/date -u -d "@${BAK_TIME}" +"%R:%S")
+echo "Total Backup Time: ${BAK_DATE}"
+echo
+
+if [ "${MOUNT_BOOT}" != "yes" ] ; then
+	echo "Unmounting /boot"
+	/bin/umount /boot
+fi
+
+if [ "${BAK_STATUS}" != 0 ] ; then
+	echo "Backup seemed to fail :("
+	echo "Exit status was: ${BAK_STATUS}"
+	false
+else
+	echo "Backup completed OK"
+	echo
+	echo "Changing from root" ; popd
+	rotate
+fi
+
+EXIT_STATUS=$?
+echo
+times
+echo
+
+return $EXIT_STATUS
 
 }
 
@@ -102,26 +119,37 @@ rotate() {
 
 cat << END
 Removing stale archives:
-Standard arachives over		$WARCHAGE days old
-Monthly arachives over		$MARCHAGE days old
+Standard arachives over		$WEEKLY_ARCHIVE_AGE days old
+Monthly arachives over		$MONTHLY_ARCHIVE_AGE days old
 
 END
 
-/usr/bin/find ${BAKSTORE} -type f -mtime +${WARCHAGE} -name "${HOSTN}-*_full-sys_*.tar.gz" -exec rm -v {} \;
-/usr/bin/find ${BAKSTORE} -type f -mtime +${WARCHAGE} -name "${HOSTN}_full-sys_*.log" -exec rm -v {} \;
-/usr/bin/find ${BAKSTORE} -type f -mtime +${MARCHAGE} -name "${HOSTN}-*_MONTHLY_*.tar.gz" -exec rm -v {} \;
-/usr/bin/find ${BAKSTORE} -type f -mtime +${MARCHAGE} -name "${HOSTN}_MONTHLY_*.log" -exec rm -v {} \;
+/usr/bin/find ${BAK_STORE} -type f -mtime +${WEEKLY_ARCHIVE_AGE} -name "${BAK_PREFIX}-*_WEEKLY_*.tar.gz" -exec rm -v {} \;
+/usr/bin/find ${BAK_STORE} -type f -mtime +${WEEKLY_ARCHIVE_AGE} -name "${BAK_PREFIX}_WEEKLY_*.log" -exec rm -v {} \;
+/usr/bin/find ${BAK_STORE} -type f -mtime +${MONTHLY_ARCHIVE_AGE} -name "${BAK_PREFIX}-*_MONTHLY_*.tar.gz" -exec rm -v {} \;
+/usr/bin/find ${BAK_STORE} -type f -mtime +${MONTHLY_ARCHIVE_AGE} -name "${BAK_PREFIX}_MONTHLY_*.log" -exec rm -v {} \;
 
 }
 
 
+exit_err() {
 
-header >> $LOG
-backup >> $LOG
+echo "ERROR: ${1} - exiting..."
+exit 1
 
-if [ $EXITSTAT -ne 0 ] ; then
+}
+
+
+[ -d "${BAK_STORE}" ] || exit_err "Backup destination does not exist"
+[ -f "${BAK_LIST}" ] || exit_err "Backup list does not exist"
+
+
+header >> "${LOG}"
+backups >> "${LOG}"
+
+if [ $EXIT_STATUS -ne 0 ] ; then
 	echo "Backup Failue"
-	cat $LOG
+	cat "${LOG}"
 fi
 
-exit $EXITSTAT
+exit $EXIT_STATUS
